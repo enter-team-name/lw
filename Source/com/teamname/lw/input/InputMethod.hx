0384bfa9b1c4db4763b5a350bf3802cf75d24c84
ilyaluk <i@ilyaluk.ru>
2014-12-31 17:45:15 +0300

    Fixed compilation error.

--- Source/com/teamname/lw/input/InputMethod.hx
+++ Source/com/teamname/lw/input/InputMethod.hx
@@ -1,8 +1,7 @@
-
 package com.teamname.lw.input;
 
 interface InputMethod {
 	public var dx(default, null) : Int;
 	public var dy(default, null) : Int;
-	public function tick(?e : MouseEvent = null) : Void;
+	public function tick() : Void;
 }
